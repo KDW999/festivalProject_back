eb42466af2452e7fb2d2c57cea9f64d3000fc136
KDW999 <hide4321@naver.com>
2023-04-27 15:05:50 +0900

    for pull

--- src/main/java/com/festival/back/service/implementation/BoardServiceImplements.java
+++ src/main/java/com/festival/back/service/implementation/BoardServiceImplements.java
@@ -235,11 +235,11 @@ public class BoardServiceImplements implements BoardService {
     // ? 특정축제 전체 후기 게시글 불러오기 -김종빈
     public ResponseDto<List<GetFestivalReviewBoardListResponseDto>> getFestivalReviewBoardList(Integer festivalNumber) {
         List<GetFestivalReviewBoardListResponseDto> data = null;
-
+    
         try {
             List<BoardEntity> boardEntityList=boardRepository.findByFestivalNumberOrderByBoardWriteDatetimeDesc(festivalNumber);
             if(boardEntityList.isEmpty()) return ResponseDto.setFail(ResponseMessage.NOT_EXIST_BOARD);
-       
+    
             data=GetFestivalReviewBoardListResponseDto.copyList(boardEntityList);
             
         } catch (Exception e) {
