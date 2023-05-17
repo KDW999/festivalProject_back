1ee6b1c08eab4042b9144d4bcb09c4df7891f720
jaehyeon <hyesung6516@naver.com>
2023-05-17 17:48:34 +0900

    자유게시물 리스트 반환 및 특정 자유게시물 반환API구현

--- src/main/java/com/festival/back/service/FreeBoardService.java
+++ src/main/java/com/festival/back/service/FreeBoardService.java
@@ -1,6 +1,8 @@
 package com.festival.back.service;
 
 
+import java.util.List;
+
 import com.festival.back.dto.request.freeboard.FreeBoardRecommendRequestDto;
 import com.festival.back.dto.request.freeboard.PatchFreeBoardCommentRequestDto;
 import com.festival.back.dto.request.freeboard.PatchFreeBoardRequestDto;
@@ -10,6 +12,8 @@ import com.festival.back.dto.response.ResponseDto;
 import com.festival.back.dto.response.freeboard.DeleteFreeBoardCommentResponseDto;
 import com.festival.back.dto.response.freeboard.DeleteFreeBoardResponseDto;
 import com.festival.back.dto.response.freeboard.FreeBoardRecommendResponseDto;
+import com.festival.back.dto.response.freeboard.GetFreeBoardListResponseDto;
+import com.festival.back.dto.response.freeboard.GetFreeBoardResponseDto;
 import com.festival.back.dto.response.freeboard.PatchFreeBoardCommentResponseDto;
 import com.festival.back.dto.response.freeboard.PatchFreeBoardResponseDto;
 import com.festival.back.dto.response.freeboard.PostFreeBoardCommentResponseDto;
@@ -18,10 +22,15 @@ import com.festival.back.dto.response.freeboard.PostFreeBoardResponseDto;
 public interface FreeBoardService{
     
     public ResponseDto<PostFreeBoardResponseDto> postFreeBoard(String userId, PostFreeBoardRequestDto requestBody);
-    public ResponseDto<PatchFreeBoardResponseDto> patchFreeBoard(String userId, PatchFreeBoardRequestDto requestBody);
-    public ResponseDto<DeleteFreeBoardResponseDto> deleteFreeBoard(String userId, int freeBoardNumber);
     public ResponseDto<PostFreeBoardCommentResponseDto> postFreeBoardComment(String userId, PostFreeBoardCommentRequestDto requestBody);
-    public ResponseDto<PatchFreeBoardCommentResponseDto> patchFreeBoardComment(String userId, PatchFreeBoardCommentRequestDto requestBody);
-    public ResponseDto<DeleteFreeBoardCommentResponseDto> deleteFreeBoardComment(String userId, int freeBoardCommentNumber);
     public ResponseDto<FreeBoardRecommendResponseDto> freeBoardRecommend(String userId, FreeBoardRecommendRequestDto requestBody);
+
+    public ResponseDto<List<GetFreeBoardListResponseDto>> getFreeBoardList();
+    public ResponseDto<GetFreeBoardResponseDto> getFreeBoard(int freeBoardNumber);
+
+    public ResponseDto<PatchFreeBoardResponseDto> patchFreeBoard(String userId, PatchFreeBoardRequestDto requestBody);
+    public ResponseDto<PatchFreeBoardCommentResponseDto> patchFreeBoardComment(String userId, PatchFreeBoardCommentRequestDto requestBody);
+
+    public ResponseDto<DeleteFreeBoardResponseDto> deleteFreeBoard(String userId, int freeBoardNumber);
+    public ResponseDto<DeleteFreeBoardCommentResponseDto> deleteFreeBoardComment(String userId, int freeBoardCommentNumber);
 }
