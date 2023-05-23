90a61001d129a64cf1b2149d28a21f4f26aa7e05
jaehyeon <hyesung6516@naver.com>
2023-05-23 15:37:33 +0900

    댓글삭제 API 수정중

--- src/main/java/com/festival/back/dto/response/board/DeleteReviewBoardResponseDto.java
+++ src/main/java/com/festival/back/dto/response/board/DeleteReviewBoardResponseDto.java
@@ -1,5 +1,11 @@
 package com.festival.back.dto.response.board;
 
+import java.util.List;
+
+import com.festival.back.entity.BoardEntity;
+import com.festival.back.entity.CommentEntity;
+import com.festival.back.entity.RecommendEntity;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -11,6 +17,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @ApiModel(value = "특정 게시물 삭제 Response Body-data")
 public class DeleteReviewBoardResponseDto {
-    @ApiModelProperty(value = "특정게시물 삭제 결과",example = "true",required = true)
-    private boolean resultStatus;
+    @ApiModelProperty(value="게시물 Entity", required=true)
+    private BoardEntity board;
+
+    @ApiModelProperty(value="추천 EntityList", required=true)
+    private List<RecommendEntity> recommendList;
+
+    @ApiModelProperty(value="댓글 EntityList", required=true)
+    private List<CommentEntity> commentList;
 }
